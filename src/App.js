d4704c23feffcbddf0b71a91cbd41d21f9a50ccc
pragyay <p.ksingh562001@gmail.com>
2024-05-16 10:13:23 +0530

    Changes

--- src/App.js
+++ src/App.js
@@ -55,41 +55,44 @@ function LiveScores() {
   
   return (
     <div>
-    <nav id='navbar'>
-      <h1>Daily Football Scores</h1>
-    </nav>
-    <div className='main'>
-      <div className="league-buttons">
-        <button className={selectedLeagues.length === 0 ? 'active' : ''} onClick={handleAllLeagues}>All</button>
-        <button className={selectedLeagues.includes(39) ? 'active' : ''} onClick={() => handleLeagueFilter(39)}>Premier League</button>
-        <button className={selectedLeagues.includes(140) ? 'active' : ''} onClick={() => handleLeagueFilter(140)}>La Liga</button>
-        <button className={selectedLeagues.includes(78) ? 'active' : ''} onClick={() => handleLeagueFilter(78)}>Bundesliga</button>
-        <button className={selectedLeagues.includes(135) ? 'active' : ''} onClick={() => handleLeagueFilter(135)}>Serie A</button>
-        <button className={selectedLeagues.includes(61) ? 'active' : ''} onClick={() => handleLeagueFilter(61)}>Ligue 1</button>
-      </div>
-  
-      {/* Display matches */}
-      {liveScores.map((fixture) => (
-        <div key={fixture.fixture.id} className="game">
-          <div className="teams">
-            <div className="teamHome">
-              <span>{fixture.teams.home.name}</span>
-              <img src={fixture.teams.home.logo} alt="Home Team Logo" className="team-logo" />
-            </div>
-            <div className="status-score">
-              <span className="score">{fixture.goals.home} - {fixture.goals.away}</span>
-              <br />
-              <span className="status">{fixture.fixture.status.long} </span>
-            </div>
-            <div className="teamAway">
-              <img src={fixture.teams.away.logo} alt="Away Team Logo" className="team-logo" />
-              <span>{fixture.teams.away.name}</span>
-            </div>
-          </div>
+      <nav id='navbar'>
+        <h1>Daily Football Scores</h1>
+      </nav>
+      <div className='main'>
+        <div className="league-buttons">
+          <button className={selectedLeagues.length === 0 ? 'active' : ''} onClick={handleAllLeagues}>All</button>
+          <button className={selectedLeagues.includes(39) ? 'active' : ''} onClick={() => handleLeagueFilter(39)}>Premier League</button>
+          <button className={selectedLeagues.includes(140) ? 'active' : ''} onClick={() => handleLeagueFilter(140)}>La Liga</button>
+          <button className={selectedLeagues.includes(78) ? 'active' : ''} onClick={() => handleLeagueFilter(78)}>Bundesliga</button>
+          <button className={selectedLeagues.includes(135) ? 'active' : ''} onClick={() => handleLeagueFilter(135)}>Serie A</button>
+          <button className={selectedLeagues.includes(61) ? 'active' : ''} onClick={() => handleLeagueFilter(61)}>Ligue 1</button>
         </div>
-      ))}
+  
+        {liveScores.length === 0 ? (
+          <div>No matches ongoing right now</div>
+        ) : (
+          liveScores.map((fixture) => (
+            <div key={fixture.fixture.id} className="game">
+              <div className="teams">
+                <div className="teamHome">
+                  <span>{fixture.teams.home.name}</span>
+                  <img src={fixture.teams.home.logo} alt="Home Team Logo" className="team-logo" />
+                </div>
+                <div className="status-score">
+                  <span className="score">{fixture.goals.home} - {fixture.goals.away}</span>
+                  <br />
+                  <span className="status">{fixture.fixture.status.long} </span>
+                </div>
+                <div className="teamAway">
+                  <img src={fixture.teams.away.logo} alt="Away Team Logo" className="team-logo" />
+                  <span>{fixture.teams.away.name}</span>
+                </div>
+              </div>
+            </div>
+          ))
+        )}
+      </div>
     </div>
-  </div>
   );
 }
 
